e67b5bf888b97009a2f8240206bcd4399d560b92
rezvoj <rezac.vojtech.0@gmail.com>
2023-12-07 18:06:36 +0100

    Added database functions

--- database.py
+++ database.py
@@ -80,3 +80,38 @@ def getStreamerStatus(connection: sqlite3.Connection, streamerId):
         if status is not None:
             return str(status[0])
         return None
+
+
+def getStreamers(connection: sqlite3.Connection):
+    with transaction(connection, readOnly=True) as cursor:
+        cursor.execute("SELECT * FROM Streamer")
+        streamers = cursor.fetchall()
+        return [(str(streamer[0]), str(streamer[1])) for streamer in streamers]
+
+
+def updateStreamerStatus(connection: sqlite3.Connection, streamerId, status):
+    with transaction(connection) as cursor:
+        cursor.execute("UPDATE Streamer SET status = ? WHERE id = ?", (int(status), int(streamerId)))
+
+
+def listForUser(connection: sqlite3.Connection, userId, streamerId = None):
+    userId = int(userId)
+    with transaction(connection, readOnly=True) as cursor:
+        if streamerId is None:
+            cursor.execute("SELECT streamerId, status FROM WatchedStatus WHERE userId = ?", (userId, ))
+        else:
+            cursor.execute("SELECT streamerId, status FROM WatchedStatus WHERE userId = ? AND streamerId = ?", (userId, int(streamerId)))
+        resultList = dict()
+        for watchedStatus in cursor.fetchall():
+            streamer, status = str(watchedStatus[0]), str(watchedStatus[1])
+            if streamer not in resultList:
+                resultList[streamer] = list()
+            resultList[streamer].append(status)
+        return resultList
+
+
+def getUsersForStreamerStatus(connection: sqlite3.Connection, streamerId, status):
+    with transaction(connection, readOnly=True) as cursor:
+        streamerId, status = int(streamerId), int(status)
+        cursor.execute("SELECT userId FROM WatchedStatus WHERE streamerId = ? AND status = ?", (streamerId, status))
+        return [str(watchedStatus[0]) for watchedStatus in cursor.fetchall()]
