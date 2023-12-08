3543fe0d9a7a32704d5cc957eb1c387eb9a318ac
rezvoj <rezac.vojtech.0@gmail.com>
2023-12-08 20:14:02 +0100

    Added database function tests

--- tests.py
+++ tests.py
@@ -87,6 +87,80 @@ class DatabaseTests(unittest.TestCase):
             self.assertEqual(watchedStatusExisting, expectedWatchedStatusExisting)
 
 
+    def testRemoveForUser(self):
+        database.initDB(self.connection)
+        userId1, userId2 = 12345, 67890
+        streamerId1, streamerId2 = 111, 222
+        status1, status2 = 1, 2
+        with database.transaction(self.connection) as cursor:
+            cursor.execute("INSERT INTO Streamer (id, status) VALUES (?, ?)", (streamerId1, 0))
+            cursor.execute("INSERT INTO Streamer (id, status) VALUES (?, ?)", (streamerId2, 0))
+            cursor.execute("INSERT INTO WatchedStatus (streamerId, userId, status) VALUES (?, ?, ?)", (streamerId1, userId1, status1))
+            cursor.execute("INSERT INTO WatchedStatus (streamerId, userId, status) VALUES (?, ?, ?)", (streamerId1, userId1, status2))
+            cursor.execute("INSERT INTO WatchedStatus (streamerId, userId, status) VALUES (?, ?, ?)", (streamerId2, userId1, status1))
+            cursor.execute("INSERT INTO WatchedStatus (streamerId, userId, status) VALUES (?, ?, ?)", (streamerId2, userId2, status1))
+        database.removeForUser(self.connection, userId1)
+        with database.transaction(self.connection, readOnly=True) as cursor:
+            cursor.execute("SELECT * FROM WatchedStatus WHERE userId = ?", (userId1,))
+            self.assertEqual(cursor.fetchall(), [])
+            cursor.execute("SELECT * FROM Streamer WHERE id = ?", (streamerId1,))
+            self.assertIsNone(cursor.fetchone())
+            cursor.execute("SELECT * FROM Streamer WHERE id = ?", (streamerId2,))
+            self.assertIsNotNone(cursor.fetchone())
+        with database.transaction(self.connection) as cursor:
+            cursor.execute("INSERT INTO WatchedStatus (streamerId, userId, status) VALUES (?, ?, ?)", (streamerId1, userId1, status1))
+            cursor.execute("INSERT INTO WatchedStatus (streamerId, userId, status) VALUES (?, ?, ?)", (streamerId1, userId1, status2))
+        database.removeForUser(self.connection, userId1, streamerId1, status1)
+        with database.transaction(self.connection, readOnly=True) as cursor:
+            cursor.execute("SELECT * FROM WatchedStatus WHERE userId = ? AND streamerId = ? AND status = ?", (userId1, streamerId1, status1))
+            self.assertEqual(cursor.fetchall(), [])
+            cursor.execute("SELECT * FROM WatchedStatus WHERE userId = ? AND streamerId = ?", (userId1, streamerId1))
+            self.assertNotEqual(cursor.fetchall(), [])
+        database.removeForUser(self.connection, userId1, streamerId2)
+        with database.transaction(self.connection, readOnly=True) as cursor:
+            cursor.execute("SELECT * FROM WatchedStatus WHERE userId = ? AND streamerId = ?", (userId1, streamerId2))
+            self.assertEqual(cursor.fetchall(), [])
+        with database.transaction(self.connection, readOnly=True) as cursor:
+            cursor.execute("SELECT * FROM Streamer WHERE id = ?", (streamerId2,))
+            self.assertIsNotNone(cursor.fetchone())
+
+
+    def testGetStreamerStatus(self):
+        database.initDB(self.connection)
+        with database.transaction(self.connection) as cursor:
+            cursor.execute("INSERT INTO Streamer (id, status) VALUES (?, ?)", (111, 5))
+        status = database.getStreamerStatus(self.connection, 111)
+        self.assertEqual(status, "5")
+        status = database.getStreamerStatus(self.connection, 222)
+        self.assertIsNone(status)
+
+
+    def testGetStreamers(self):
+        database.initDB(self.connection)
+        with database.transaction(self.connection) as cursor:
+            cursor.execute("INSERT INTO Streamer (id, status) VALUES (?, ?)", (111, 5))
+            cursor.execute("INSERT INTO Streamer (id, status) VALUES (?, ?)", (222, 10))
+        streamers = database.getStreamers(self.connection)
+        expectedStreamers = [("111", "5"), ("222", "10")]
+        self.assertEqual(streamers, expectedStreamers)
+
+
+    def testUpdateStreamerStatus(self):
+        database.initDB(self.connection)
+        with database.transaction(self.connection) as cursor:
+            cursor.execute("INSERT INTO Streamer (id, status) VALUES (?, ?)", (111, 5))
+        database.updateStreamerStatus(self.connection, 111, 20)
+        with database.transaction(self.connection, readOnly=True) as cursor:
+            cursor.execute("SELECT status FROM Streamer WHERE id = ?", (111,))
+            status = cursor.fetchone()[0]
+            self.assertEqual(status, 20)
+        database.updateStreamerStatus(self.connection, 333, 15)
+        with database.transaction(self.connection, readOnly=True) as cursor:
+            cursor.execute("SELECT * FROM Streamer WHERE id = ?", (333,))
+            result = cursor.fetchone()
+            self.assertIsNone(result)
+
+
 
 if __name__ == '__main__':
     unittest.main()
